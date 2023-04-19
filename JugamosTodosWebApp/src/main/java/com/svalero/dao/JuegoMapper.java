4fa51a3ba6905edc8fc89e2573d41fbe2f94f931
RubenLCgit <125395496+RubenLCgit@users.noreply.github.com>
2023-04-19 21:10:38 +0200

    fix: Fix bug in getClubs method

--- JugamosTodosWebApp/src/main/java/com/svalero/dao/JuegoMapper.java
+++ JugamosTodosWebApp/src/main/java/com/svalero/dao/JuegoMapper.java
@@ -10,6 +10,6 @@ import java.sql.SQLException;
 public class JuegoMapper implements RowMapper<Juego> {
     @Override
     public Juego map(ResultSet rs, StatementContext ctx) throws SQLException {
-        return new Juego(rs.getString("NOMBRE"), rs.getInt("MAX_JUGADORES"), rs.getString("TIPO"), rs.getInt("DURACION_MIN"));
+        return new Juego(rs.getString("ID_JUEGO"),rs.getString("NOMBRE"), rs.getInt("MAX_JUGADORES"), rs.getString("TIPO"), rs.getInt("DURACION_MIN"));
     }
 }
